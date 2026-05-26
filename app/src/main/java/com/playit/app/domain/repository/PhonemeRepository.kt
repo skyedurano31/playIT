885b8c950e2466478974e296795458fc9ee95a7f
skyedurano <ashleeeskye@gmail.com>
2026-05-26 10:12:43 +0800

    methods for phoneme repository

--- app/src/main/java/com/playit/app/domain/repository/PhonemeRepository.kt
+++ app/src/main/java/com/playit/app/domain/repository/PhonemeRepository.kt
@@ -1,4 +1,8 @@
 package com.playit.app.domain.repository
 
+import com.playit.app.data.local.entity.Phoneme
+
 interface PhonemeRepository {
+    suspend fun getAllPhonemesOrdered(): List<Phoneme>
+    suspend fun getPhonemeById(phonemeId: Int): Phoneme?
 }
