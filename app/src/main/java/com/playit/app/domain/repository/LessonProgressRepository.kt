7e8058aeee1dbcf092605bf2127607252773ffe9
skyedurano <ashleeeskye@gmail.com>
2026-05-26 10:13:11 +0800

    methods for lessonprog repository

--- app/src/main/java/com/playit/app/domain/repository/LessonProgressRepository.kt
+++ app/src/main/java/com/playit/app/domain/repository/LessonProgressRepository.kt
@@ -1,4 +1,11 @@
 package com.playit.app.domain.repository
 
+import com.playit.app.data.local.entity.LessonProgress
+
 interface LessonProgressRepository {
+    suspend fun insert(lessonProgress: LessonProgress)
+    suspend fun update(lessonProgress: LessonProgress)
+    suspend fun getProgressByProfile(profileId: Int): List<LessonProgress>
+    suspend fun getProgress(profileId: Int, phonemeId: Int): LessonProgress?
+    suspend fun isCompleted(profileId: Int, phonemeId: Int): Boolean
 }
