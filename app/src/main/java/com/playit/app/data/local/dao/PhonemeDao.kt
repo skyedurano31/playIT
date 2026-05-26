775af0868b61ea1082e03eff24d032b960df2539
skyedurano <ashleeeskye@gmail.com>
2026-05-26 10:10:56 +0800

    add queries for phonemeDAO

--- app/src/main/java/com/playit/app/data/local/dao/PhonemeDao.kt
+++ app/src/main/java/com/playit/app/data/local/dao/PhonemeDao.kt
@@ -3,11 +3,18 @@ package com.playit.app.data.local.dao;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
+import androidx.room.Query
 
 import com.playit.app.data.local.entity.Phoneme;
 
 @Dao
 public interface PhonemeDao {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun insertAll(phonemes:List<Phoneme>)
+    suspend fun insertAll(phonemes: List<Phoneme>)
+
+    @Query("SELECT * FROM phoneme ORDER BY phonemeId ASC")
+    suspend fun getAllPhonemesOrdered(): List<Phoneme>
+
+    @Query("SELECT * FROM phoneme WHERE phonemeId = :phonemeId")
+    suspend fun getPhonemeById(phonemeId: Int): Phoneme?
 }
