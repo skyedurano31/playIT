3f9d12d17cd97569315349c99b1b12d7b9c29ead
skyedurano <ashleeeskye@gmail.com>
2026-05-26 10:16:01 +0800

    implement repository in phonemerepoIMPL

--- app/src/main/java/com/playit/app/data/repository/PhonemeRepositoryImpl.kt
+++ app/src/main/java/com/playit/app/data/repository/PhonemeRepositoryImpl.kt
@@ -1,9 +1,18 @@
 package com.playit.app.data.repository
 
 import com.playit.app.data.local.dao.PhonemeDao
+import com.playit.app.data.local.entity.Phoneme
 import com.playit.app.domain.repository.PhonemeRepository
 import javax.inject.Inject
 
 class PhonemeRepositoryImpl @Inject constructor(
     private val phonemeDao: PhonemeDao
-) : PhonemeRepository
+) : PhonemeRepository {
+    override suspend fun getAllPhonemesOrdered(): List<Phoneme> {
+        return phonemeDao.getAllPhonemesOrdered()
+    }
+
+    override suspend fun getPhonemeById(phonemeId: Int): Phoneme? {
+        return phonemeDao.getPhonemeById(phonemeId)
+    }
+}
